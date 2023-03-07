0bf1b5c75fe71ec28ded144910644723cf6db92a
Timyn <tbalogun@ggc.edu>
2023-03-07 16:40:37 -0500

    JobApplicationProject

--- src/PasswordChecker.java
+++ src/PasswordChecker.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class PasswordChecker {
+import java.util.Scanner;
+
+public class PasswordChecker {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a password: ");
+        String password = input.nextLine();
+
+        if (isSecurePassword(password)) {
+            System.out.println("The password is secure.");
+        } else {
+            System.out.println("The password is not secure.");
+        }
+    }
+
+    public static boolean isSecurePassword(String password) {
+        // Check if password length is at least 8 characters
+        if (password.length() < 8) {
+            return false;
+        }
+
+        // Check if password contains at least one uppercase letter
+        if (!password.matches(".*[A-Z].*")) {
+            return false;
+        }
+
+        // Check if password contains at least one lowercase letter
+        if (!password.matches(".*[a-z].*")) {
+            return false;
+        }
+
+        // Check if password contains at least one digit
+        if (!password.matches(".*\\d.*")) {
+            return false;
+        }
+
+        // Check if password contains at least one special character
+        if (!password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) {
+            return false;
+        }
+
+        // If all checks pass, return true
+        return true;
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/UsernameChecker.java
+++ src/UsernameChecker.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class UsernameChecker {
+import java.util.Scanner;
+
+public class UsernameChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your username: ");
+        String username = input.nextLine();
+
+        if (isSecureUsername(username)) {
+            System.out.println("Your username is secure.");
+        } else {
+            System.out.println("Your username is not secure.");
+        }
+    }
+
+    public static boolean isSecureUsername(String username) {
+        if (username.length() < 8 || username.length() > 20) {
+            return false;
+        }
+
+        if (!username.matches(".*[a-zA-Z].*")) {
+            return false;
+        }
+
+        if (!username.matches(".*[0-9].*")) {
+            return false;
+        }
+
+        if (!username.matches("^[a-zA-Z0-9]*$")) {
+            return false;
+        }
+
+        return true;
+    }
 }
